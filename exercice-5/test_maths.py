447d69a66d3e403e414952d6e7d9591f0c670d4a
RedhaMebarki <redha.mebarki93@gmail.com>
2024-05-07 01:00:52 +0200

    Exerice 5 fait

--- exercice-5/test_maths.py
+++ exercice-5/test_maths.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def add(x, y):
     """Addition function."""
     return x + y
@@ -19,3 +22,17 @@ def divide(x, y):
 """écrire les tests unitaires pour ces fonctions.
 atteindre un coverage de 100%
 """
+
+def test_add():
+    assert add(5,2) == 7
+def test_subtract():
+    assert subtract(5,2) == 3
+def test_multiply():
+    assert multiply(5,2) == 10
+def test_divide():
+    
+    with pytest.raises(ValueError) as e:
+        divide(10, 0)
+    assert str(e.value) == "Cannot divide by zero!"
+    assert divide(10,2) == 5
+    assert divide(5,2) == 2.5
